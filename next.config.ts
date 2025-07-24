a7ba82b0fc20674e074d7ddced701b275665859b
exelimpichment <oldestspy@gmail.com>
2025-07-24 21:25:35 +0200

    add SVG loader support in Next.js configuration; enhance webpack setup to handle SVG files with @svgr/webpack

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,30 @@ const nextConfig = {
     reactCompiler: true,
     viewTransition: true,
   },
+  webpack(config: {
+    module?: {
+      rules?: Array<unknown>;
+    };
+    [key: string]: unknown;
+  }) {
+    if (config.module?.rules) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        issuer: { and: [/\.(?:tsx?|jsx?)$/] },
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              icon: true,
+            },
+          },
+        ],
+      });
+    }
+
+    return config;
+  },
+
   turbopack: {
     rules: {
       '*.svg': {
